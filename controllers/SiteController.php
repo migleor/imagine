91cd7f6aa03d010dbfbb6edf98ac889362197edc
Leonardo Ortega <migleor@gmail.com>
2020-07-02 14:32:02 -0500

    action saluda

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -15,6 +15,11 @@ class SiteController extends Controller
     /**
      * {@inheritdoc}
      */
+    public function actionSaluda()
+    {
+        return $this->render("saluda");
+    }
+
     public function behaviors()
     {
         return [
